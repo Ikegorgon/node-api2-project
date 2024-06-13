7cab504ccc017403822b3dd43c4b1e5898e0c805
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2024-06-13 15:53:25 -0600

    Project Completion

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const server = express();
 const postsRouter = require("./posts/posts-router");
 
+server.use(express.json());
 server.use("/api/posts", postsRouter);
 
 module.exports = server;
